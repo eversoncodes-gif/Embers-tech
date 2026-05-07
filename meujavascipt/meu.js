d648eb49f2ac39c37b1ddef533ec39cfed55e088
Everson Muianga <eversoncodes@gmail.com>
2026-05-07 20:41:46 +0200

    adiconei menu amburguer

--- meujavascipt/meu.js
+++ meujavascipt/meu.js
@@ -1,1 +1,6 @@
-function 
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-bar");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
