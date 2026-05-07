8357f561208e73050a82b37ac155cb0e68be5f22
Everson Muianga <eversoncodes@gmail.com>
2026-05-07 21:03:00 +0200

    coloquei menu hamburguer

--- meujavascipt/meu.js
+++ meujavascipt/meu.js
@@ -1,6 +1,9 @@
+function alertaMenu(){
+    alert ("still adapting the menu")
+}
 const menu = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav-bar");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", () => { 
     nav.classList.toggle("active");
 });
