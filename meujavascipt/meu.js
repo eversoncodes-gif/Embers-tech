65d6b3a8d25df456246012d0217e805cf7213714
Everson Muianga <eversoncodes@gmail.com>
2026-05-10 15:09:33 +0200

    adicionei js

--- meujavascipt/meu.js
+++ meujavascipt/meu.js
@@ -1,9 +1,12 @@
-function alertaMenu(){
-    alert ("still adapting the menu")
-}
+// aqui e a parte do menu, ainda em desenvolvimento, cosnt e uma variavel ainda em descobrimento
 const menu = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav-bar");
 
-menu.addEventListener("click", () => { 
+//aqui e o que acontece as variaveis ja criadas
+menu.addEventListener("click",() =>{
     nav.classList.toggle("active");
-});
+    
+    if(nav.classList.contains("active")){
+        alert("Still adapting menu");
+    }
+}); 
